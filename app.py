946e8a0ee977372728a3fde78ce65d7668110808
Om Mishra <om.mishra@bacancy.com>
2024-12-30 12:35:52 +0530

    fix bug

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, PythonAnywhere!"
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#     return "Hello, PythonAnywhere!"
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
